cf4ac62f997dad81460a1f21535438f829cec77f
Andre <dat.andre.on.juno@gmail.com>
2024-11-13 08:17:02 +0000

    class 12_11_2024

---
+++ programs/amm/src/instructions/deposit.rs
@@ -0,0 +1,82 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{associated_token::AssociatedToken, token::{Mint, Token, TokenAccount}};
+
+use crate::Config;
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub mint_x: Account<'info, Mint>,
+    pub mint_y: Account<'info, Mint>,
+    #[account(
+        mut,
+        has_one = mint_x,
+        has_one = mint_y,
+        seeds = [b"config", config.seed.to_le_bytes().as_ref()],
+        bump = config.config_bump,
+    )]
+    pub config: Account<'info, Config>,
+    #[account(
+        mut,
+        seeds = [b"lp", config.key().as_ref()],
+        bump = config.lp_bump,
+    )]
+    pub mint_lp: Account<'info, Mint>,
+    #[account(
+        mut,
+        associated_token::mint = mint_x,
+        associated_token::authority = config,
+    )]
+    pub vault_x: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = mint_y,
+        associated_token::authority = config,
+    )]
+    pub vault_y: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = mint_x,
+        associated_token::authority = user,
+    )]
+    pub user_x: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = mint_y,
+        associated_token::authority = user,
+    )]
+    pub user_y: Account<'info, TokenAccount>,
+    #[account(
+        init_if_needed,
+        payer = user,
+        associated_token::mint = mint_lp,
+        associated_token::authority = user,
+    )]
+    pub user_lp: Account<'info, TokenAccount>,
+   
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+impl<'info> Deposit<'info> {
+    pub fn deposit(&mut self, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+
+        require!(self.config.locked == false, ErrorCode::CustomError);
+        require!(self.user_lp.amount >= amount, ErrorCode::CustomError);
+
+        let(x, y) = match self.mint_lp.supply == 0 && self.vault_x.amount == 0 && self.vault_y.amount == 0 {
+            true => (max_x, max_y),
+            false => {
+                let amounts = Constant
+                let x = amount * self.vault_x.amount / self.mint_lp.supply;
+                let y = amount * self.vault_y.amount / self.mint_lp.supply;
+                (x, y)
+            }
+        };
+        Ok(())
+    }
+    
+}
+
